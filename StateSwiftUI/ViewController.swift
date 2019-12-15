e4b1771deba5973597986747927c8ec1e00cf57b
arnoldtjiawi <arnoldtjiawi@gmail.com>
2019-12-15 20:36:53 +0700

    Initial Commit

---
+++ StateSwiftUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StateSwiftUI
+//
+//  Created by Arnold Tjiawi on 15/12/19.
+//  Copyright © 2019 ArnoldTjiawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
